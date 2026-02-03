030b85974a391386f357164abab2ee98f9722142
Ádrian Gama <adrian.gama.se@gmail.com>
2026-02-03 15:46:43 -0300

    Messily fix cuda build for sycl

--- src/sycl/src/benchmark.cpp
+++ src/sycl/src/benchmark.cpp
@@ -1,4 +1,3 @@
-#include <array>
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
@@ -163,6 +162,12 @@ void benchmark(VglImage* image, size_t rounds, std::function<void(VglImage*, cha
     auto input = Image(d_input_data, d_shape, d_offset, dimensions, image_size);
     auto output = Image(d_output_data, d_shape, d_offset, dimensions, image_size);
     auto aux = Image(d_aux_data, d_shape, d_offset, dimensions, image_size);
+    auto d_input = sycl::malloc_device<Image>(sizeof(Image), q);
+    auto d_output = sycl::malloc_device<Image>(sizeof(Image), q);
+    auto d_aux = sycl::malloc_device<Image>(sizeof(Image), q);
+    q.memcpy(d_input, &input, sizeof(Image)).wait();
+    q.memcpy(d_output, &output, sizeof(Image)).wait();
+    q.memcpy(d_aux, &aux, sizeof(Image)).wait();
 
     auto strel_cross = new VglStrEl(VGL_STREL_CROSS, dimensions);
     auto strel_cube = new VglStrEl(VGL_STREL_CUBE, dimensions);
@@ -184,12 +189,20 @@ void benchmark(VglImage* image, size_t rounds, std::function<void(VglImage*, cha
     auto window_cross = Window(d_window_cross_data, d_window_shape, d_window_offset, dimensions, window_size);
     auto window_cube = Window(d_window_cube_data, d_window_shape, d_window_offset, dimensions, window_size);
     auto window_mean = Window(d_window_mean_data, d_window_shape, d_window_offset, dimensions, window_size);
+    auto d_window_cross = sycl::malloc_device<Window>(sizeof(Window), q);
+    auto d_window_cube = sycl::malloc_device<Window>(sizeof(Window), q);
+    auto d_window_mean = sycl::malloc_device<Window>(sizeof(Window), q);
+    q.memcpy(d_window_cross, &window_cross, sizeof(Window)).wait();
+    q.memcpy(d_window_cube, &window_cube, sizeof(Window)).wait();
+    q.memcpy(d_window_mean, &window_mean, sizeof(Window)).wait();
 
-    std::array<Window, VGL_ARR_SHAPE_SIZE> window_cube_array;
-    std::array<Window, VGL_ARR_SHAPE_SIZE> window_mean_array;
+    Window window_cube_array[VGL_ARR_SHAPE_SIZE];
+    Window window_mean_array[VGL_ARR_SHAPE_SIZE];
     auto window_linear_size = 3 * sizeof(float);
     float window_cube_data_linear[] = { 1.0f, 1.0f, 1.0f };
     float window_mean_data_linear[] = { 1.0f / 3.0f, 1.0f / 3.0f, 1.0f / 3.0f };
+    Window* window_cube_array_d[VGL_ARR_SHAPE_SIZE];
+    Window* window_mean_array_d[VGL_ARR_SHAPE_SIZE];
     auto d_window_cube_linear_data = sycl::malloc_device<float>(window_linear_size, q);
     auto d_window_mean_linear_data = sycl::malloc_device<float>(window_linear_size, q);
     q.memcpy(d_window_cube_linear_data, window_cube_data_linear, window_linear_size).wait();
@@ -208,7 +221,11 @@ void benchmark(VglImage* image, size_t rounds, std::function<void(VglImage*, cha
         q.memcpy(d_window_linear_offset, vgl_shape->offset, dimensions_size).wait();
 
         window_cube_array[i] = Window(d_window_cube_linear_data, d_window_linear_shape, d_window_linear_offset, dimensions, window_linear_size);
+        window_cube_array_d[i] = sycl::malloc_device<Window>(sizeof(Window), q);
+        q.memcpy(window_cube_array_d[i], &window_cube_array[i], sizeof(Window)).wait();
         window_mean_array[i] = Window(d_window_mean_linear_data, d_window_linear_shape, d_window_linear_offset, dimensions, window_linear_size);
+        window_mean_array_d[i] = sycl::malloc_device<Window>(sizeof(Window), q);
+        q.memcpy(window_mean_array_d[i], &window_mean_array[i], sizeof(Window)).wait();
 
         delete vgl_shape;
         window_linear_shape[i] = 1;
@@ -223,48 +240,47 @@ void benchmark(VglImage* image, size_t rounds, std::function<void(VglImage*, cha
     builder.attach({ .name = "upload", .func = [&] { q.memcpy(d_input_data, image->getImageData(), image_size).wait(); } });
     builder.attach({ .name = "download", .func = [&] { q.memcpy(tmp->getImageData(), d_input_data, image_size).wait(); } });
     builder.attach({ .name = "copy", .func = [&] { q.memcpy(d_output_data, d_input_data, image_size).wait(); }, .post = save_sample });
-    builder.attach({ .name = "invert", .func = [&] { q.parallel_for(image_size, InvertKernel(&input, &output)).wait(); }, .post = save_sample });
-    builder.attach({ .name = "threshold", .func = [&] { q.parallel_for(image_size, ThresholdKernel(&input, &output, 128, 255)).wait(); }, .post = save_sample });
-    builder.attach({ .name = "erode-cross", .func = [&] { q.parallel_for(image_size, ErodeKernel(&input, &output, &window_cross)).wait(); }, .post = save_sample });
-    builder.attach({ .name = "erode-cube", .func = [&] { q.parallel_for(image_size, ErodeKernel(&input, &output, &window_cube)).wait(); }, .post = save_sample });
+    builder.attach({ .name = "invert", .func = [&] { q.parallel_for(image_size, InvertKernel(d_input, d_output)).wait(); }, .post = save_sample });
+    builder.attach({ .name = "threshold", .func = [&] { q.parallel_for(image_size, ThresholdKernel(d_input, d_output, 128, 255)).wait(); }, .post = save_sample });
+    builder.attach({ .name = "erode-cross", .func = [&] { q.parallel_for(image_size, ErodeKernel(d_input, d_output, d_window_cross)).wait(); }, .post = save_sample });
+    builder.attach({ .name = "erode-cube", .func = [&] { q.parallel_for(image_size, ErodeKernel(d_input, d_output, d_window_cube)).wait(); }, .post = save_sample });
     builder.attach({
         .name = "split-erode-cube",
         .func = [&] {
-            q.parallel_for(image_size, ErodeKernel(&input, &aux, &window_cube_array[1])).wait();
+            q.parallel_for(image_size, ErodeKernel(d_input, d_aux, window_cube_array_d[1])).wait();
             for (int i = 2; i <= dimensions; ++i)
                 if (i & 0b1)
-                    q.parallel_for(image_size, ErodeKernel(&output, &aux, &window_cube_array[i])).wait();
+                    q.parallel_for(image_size, ErodeKernel(d_output, d_aux, window_cube_array_d[i])).wait();
                 else
-                    q.parallel_for(image_size, ErodeKernel(&aux, &output, &window_cube_array[i])).wait();
+                    q.parallel_for(image_size, ErodeKernel(d_aux, d_output, window_cube_array_d[i])).wait();
             if (dimensions & 0b1)
-                q.memcpy(aux.data, output.data, image_size).wait();
+                q.memcpy(d_aux_data, d_output_data, image_size).wait();
         },
         .post = save_sample,
     });
     builder.attach({
         .name = "convolve",
-        .func = [&] { q.parallel_for(image_size, ConvolveKernel(&input, &output, &window_mean)).wait(); },
+        .func = [&] { q.parallel_for(image_size, ConvolveKernel(d_input, d_output, d_window_mean)).wait(); },
         .post = save_sample,
     });
     builder.attach({
         .name = "split-convolve",
         .func = [&] {
-            q.parallel_for(image_size, ConvolveKernel(&input, &aux, &window_mean_array[1])).wait();
+            q.parallel_for(image_size, ConvolveKernel(d_input, d_aux, window_mean_array_d[1])).wait();
             for (int i = 2; i <= dimensions; ++i)
                 if (i & 0b1)
-                    q.parallel_for(image_size, ConvolveKernel(&output, &aux, &window_mean_array[i]))
+                    q.parallel_for(image_size, ConvolveKernel(d_output, d_aux, window_mean_array_d[i]))
                         .wait();
                 else
-                    q.parallel_for(image_size, ConvolveKernel(&aux, &output, &window_mean_array[i]))
+                    q.parallel_for(image_size, ConvolveKernel(d_aux, d_output, window_mean_array_d[i]))
                         .wait();
             if (dimensions & 0b1)
-                q.memcpy(aux.data, output.data, image_size).wait();
+                q.memcpy(d_aux_data, d_output_data, image_size).wait();
         },
         .post = save_sample,
     });
 
     builder.run(rounds);
-
     sycl::free(d_window_cross_data, q);
     sycl::free(d_window_cube_data, q);
     sycl::free(d_window_mean_data, q);
@@ -276,9 +292,17 @@ void benchmark(VglImage* image, size_t rounds, std::function<void(VglImage*, cha
     sycl::free(d_output_data, q);
     sycl::free(d_window_cube_linear_data, q);
     sycl::free(d_window_mean_linear_data, q);
+    sycl::free(d_window_cross, q);
+    sycl::free(d_window_cube, q);
+    sycl::free(d_window_mean, q);
+    sycl::free(d_input, q);
+    sycl::free(d_output, q);
+    sycl::free(d_aux, q);
     for (auto i = 1; i <= dimensions; ++i) {
         sycl::free(window_cube_array[i].shape, q);
         sycl::free(window_cube_array[i].offset, q);
+        sycl::free(window_cube_array_d[i], q);
+        sycl::free(window_mean_array_d[i], q);
     }
     delete strel_cross;
     delete strel_cube;

c8fe2631552a0b69054eea54df28a8b91b9aa2a8
Ádrian Gama <adrian.gama.se@gmail.com>
2026-02-16 14:15:59 -0300

    Organize output to own dimension folder

--- matlab/run.sh
+++ matlab/run.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 PRELUDE="warning('off','all'); parallel.gpu.enableCUDAForwardCompatibility(true);"
-OUTPUT_FOLDER="./results"
-RESULT_FOLDER="../results"
+CSV_FILENAME="benchmark.csv"
+LOG_FILENAME="benchmark.log"
+OUTPUT_FOLDER_BASE="../results"
 IMAGE_PATTERN="../assets/mitosis/mitosis-5d%04d.tif"
 INDEX_0=0
 INDEX_N=335
 ROUNDS=${1:-0}
 
-mkdir -p "$RESULT_FOLDER"
-
-# echo "Running MATLAB 1D benchmark"
-# matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER\", 22020096);"           > "$RESULT_FOLDER/matlab-1d.csv" 2> "$RESULT_FOLDER/matlab-1d.log"
+OUTPUT_FOLDER=$OUTPUT_FOLDER_BASE/matlab
+mkdir -p "$OUTPUT_FOLDER/1D" "$OUTPUT_FOLDER/2D" "$OUTPUT_FOLDER/3D" "$OUTPUT_FOLDER/4D" "$OUTPUT_FOLDER/5D"
+echo "Running MATLAB 1D benchmark"
+matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER/1D\", 22020096);"           > "$OUTPUT_FOLDER/1D/$CSV_FILENAME" 2> "$OUTPUT_FOLDER/1D/$LOG_FILENAME"
 echo "Running MATLAB 2D benchmark"
-matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER\", 256, 86016);"         > "$RESULT_FOLDER/matlab-2d.csv" 2> "$RESULT_FOLDER/matlab-2d.log"
+matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER/2D\", 256, 86016);"         > "$OUTPUT_FOLDER/2D/$CSV_FILENAME" 2> "$OUTPUT_FOLDER/2D/$LOG_FILENAME"
 echo "Running MATLAB 3D benchmark"
-matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER\", 256, 256, 336);"      > "$RESULT_FOLDER/matlab-3d.csv" 2> "$RESULT_FOLDER/matlab-3d.log"
+matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER/3D\", 256, 256, 336);"      > "$OUTPUT_FOLDER/3D/$CSV_FILENAME" 2> "$OUTPUT_FOLDER/3D/$LOG_FILENAME"
 echo "Running MATLAB 4D benchmark"
-matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER\", 256, 256, 2, 168);"   > "$RESULT_FOLDER/matlab-4d.csv" 2> "$RESULT_FOLDER/matlab-4d.log"
+matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER/4D\", 256, 256, 2, 168);"   > "$OUTPUT_FOLDER/4D/$CSV_FILENAME" 2> "$OUTPUT_FOLDER/4D/$LOG_FILENAME"
 echo "Running MATLAB 5D benchmark"
-matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER\", 256, 256, 2, 24, 7);" > "$RESULT_FOLDER/matlab-5d.csv" 2> "$RESULT_FOLDER/matlab-5d.log"
-
-echo "Results and logs saved in $(realpath $RESULT_FOLDER)"
+matlab -batch "$PRELUDE benchmark(\"$IMAGE_PATTERN\", $INDEX_0, $INDEX_N, $ROUNDS, \"$OUTPUT_FOLDER/5D\", 256, 256, 2, 24, 7);" > "$OUTPUT_FOLDER/5D/$CSV_FILENAME" 2> "$OUTPUT_FOLDER/5D/$LOG_FILENAME"
+echo "Results and logs saved in $(realpath $OUTPUT_FOLDER)"
